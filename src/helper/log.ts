e9c8f6593090c758f25dbf4df431bdd8335a41d2
Pac <public@pa0x43.me>
2021-08-22 15:17:12 +0200

    Removed Sentry from Winston

--- src/helper/log.ts
+++ src/helper/log.ts
@@ -1,23 +1,13 @@
 import  { createLogger, format, transports }  from 'winston';
 const { combine, timestamp, label, prettyPrint, colorize } = format;
-import WinstonSentry from 'winston-transport-sentry-node';
 import * as dotenv from 'dotenv';
 
 const printFormat = combine(label({ label: 'right meow!' }), timestamp(), prettyPrint(), colorize())
 
 dotenv.config();
 
-const options = {
-  sentry: {
-    dsn: process.env.SENTRY_DSN,
-    release: "pr0gramm_music@" + process.env.npm_package_version,
-  },
-  level: 'error'
-};
-
 const logger = createLogger({
   transports: [
-    new WinstonSentry(options),
     new transports.Console({level: 'debug', format: printFormat})
   ]
 });
