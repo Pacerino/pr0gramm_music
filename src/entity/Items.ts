c75f5213c5e4fc939cd03ef8ac2964f6c78f2daa
Pac <public@pa0x43.me>
2021-09-02 01:17:12 +0200

    Fixed UpdatedAt, CreatedAt, DeletedAt

--- src/entity/Items.ts
+++ src/entity/Items.ts
@@ -1,4 +1,4 @@
-import {Column, Entity, Generated, PrimaryColumn } from "typeorm";
+import {Column, Entity, Generated, PrimaryColumn, CreateDateColumn, UpdateDateColumn, Index} from "typeorm";
 
 @Entity()
 export class Items {
@@ -6,6 +6,16 @@ export class Items {
     @PrimaryColumn("bigint", { width: 20, unsigned: true})
     @Generated()
     id: number;
+
+    @CreateDateColumn({ type: "datetime", width: 3, nullable: true, default: () => null })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: "datetime", width: 3, nullable: true, default: () => null })
+    updated_at: Date;
+
+    @Index("idx_items_deleted_at")
+    @Column({ type: "datetime",  width: 3, nullable: true, default: () => null})
+    deleted_at: Date
 
     @Column({type: "bigint", width: 20})
     itemID: number;
