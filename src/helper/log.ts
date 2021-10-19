08656d0f48381aed8f78f3babfda944788971822
Pac <public@pa0x43.me>
2021-10-19 23:45:30 +0200

    Removed Sentry and added Winston HTTP

--- src/helper/log.ts
+++ src/helper/log.ts
@@ -1,5 +1,4 @@
 import  { createLogger, format, transports }  from 'winston';
-import Sentry from 'winston-transport-sentry-node';
 const { combine, timestamp, prettyPrint, colorize } = format;
 import * as dotenv from 'dotenv';
 
@@ -7,17 +6,14 @@ const printFormat = combine(timestamp(), prettyPrint(), colorize())
 
 dotenv.config();
 
-const sentryTransport = new Sentry({
-  level: process.env.LOG_LEVEL,
-  sentry: {
-    release: "pr0gramm_music@" + process.env.npm_package_version,
-  }
-});
-
 const logger = createLogger({
   transports: [
     new transports.Console({level: 'debug', format: printFormat}),
-    sentryTransport
+    new transports.Http({
+      host: process.env.WINSTON_HTTP,
+      port: Number(process.env.WINSTON_PORT),
+      path: "/"
+  })
   ]
 });
 
